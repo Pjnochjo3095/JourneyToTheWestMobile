18577037f3abc0b054c8b89e517065f0a3d94545
nhutnhminh30 <nhut.nh.minh@gmail.com>
2021-05-19 12:57:27 +0700

    update-code

---
+++ build/app/generated/source/buildConfig/debug/nhutnhm/se130162/journey_to_west/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package nhutnhm.se130162.journey_to_west;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "nhutnhm.se130162.journey_to_west";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
